cdab654432df54dd5cf8d7e2d76adbf6c41eaa82
davidhcefx <davidhu0903ex3@gmail.com>
2018-04-13 12:16:38 +0800

    Create CMD sucks Use Linux.cmd
    created in 2017/05/11

---
+++ CMD sucks Use Linux.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+echo.
+call :draw "*" 40
+echo.
+echo davidhcefx
+whoami
+date /T
+echo "     ___                 __      "    
+echo "    /       |\     /|   |  \     "
+echo "   |        |  \_/  |   |   |    "
+echo "   |        |       |   |   |    "
+echo "    \___    |       |   |__/     "
+echo "                                 "
+echo         Cmd Sucks, Use Linux
+call :draw "*" 40
+echo.
+echo.
+pause
+goto :eof
+
+:draw
+ setlocal
+ set i=0
+ :loop
+  <nul set /p=%1
+  set /A i=i+1
+  if %i%==%2 ( goto :end
+  ) else ( goto :loop )
+ :end
+endlocal
